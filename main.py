e9d6a8ec893c3662c17505ce41253df0a94b69da
Sujal Dodhiya <91745415+sujaldodhiya@users.noreply.github.com>
2021-10-01 20:43:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import subprocess
 
 data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8').split('\n')  # Command to get information about all the saved wifi in a device
 
-wifis = [line.split(':')[1][1:-1] for line in data if "All User Profile" in line]  # Split the name and passwords
+wifis = [line.split(':')[1][1:-1] for line in data if "All User Profile" in line]  # Splits wifi profiles
 
 for wifi in wifis:
     results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', wifi, 'key=clear']).decode('utf-8').split('\n')
-    results = [line.split(':')[1][1:-1] for line in results if "Key Content" in line]
+    results = [line.split(':')[1][1:-1] for line in results if "Key Content" in line]  # Splits username and password
 
     try:
         print(f'Name: {wifi}, Password: {results}')
